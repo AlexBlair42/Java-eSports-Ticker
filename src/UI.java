6b23009bf8fbfea68358808788e81fab5e7746eb
Alex Blair <ablair18@my.whitworth.edu>
2017-02-27 16:42:24 -0800

    Set-Up project in IntelliJ IDE
    Set up the project in IntelliJ and created a UI class to begin with.

---
+++ src/UI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This is the UI class for our Java eSports Ticker
+ * @version 0.1.0
+ * @author Alex Blair
+ * 2/27/17
+ */
+public class UI extends JComponent{
+
+    static JFrame frame = new JFrame();
+    static void main(String args[]) {
+
+    }
+}
